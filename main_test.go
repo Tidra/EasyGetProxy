0dee1be4056b495fd28b35ca8adbe5f07ed6fbdd
Tid <349382581@qq.com>
2024-03-04 09:24:58 +0800

    deleted:    .github/workflows/jekyll-docker.yml 	modified:   main_test.go

--- main_test.go
+++ main_test.go
@@ -4,11 +4,10 @@ import (
 	"testing"
 
 	"github.com/Tidra/EasyGetProxy/app"
-	"github.com/Tidra/EasyGetProxy/web"
 )
 
 func Test_main(t *testing.T) {
 	go app.CrawlTask()
 	go app.Cron() // 定时运行
-	web.StarWeb()
+	// web.StarWeb()
 }
