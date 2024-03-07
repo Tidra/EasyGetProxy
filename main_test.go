3ccf5b09b4d7949dbfd89786b600f3ac8d675c12
Tid <349382581@qq.com>
2024-03-07 17:08:52 +0800

    更新测试程序

--- main_test.go
+++ main_test.go
@@ -3,11 +3,14 @@ package main
 import (
 	"testing"
 
+	"github.com/Tidra/EasyGetProxy/app"
 	"github.com/Tidra/EasyGetProxy/web"
 )
 
 func Test_main(t *testing.T) {
-	mainInit()
+	// go app.Cron() // 定时运行
+	app.CrawlTask()
+	app.SpeedCheckTask()
 	web.StarWeb()
 	web.WebShutdown()
 }
